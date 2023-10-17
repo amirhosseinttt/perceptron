1a7cfc6405deef1ca18af5d958b5dd9c41360f2e
amirhossein <torkanlooa@gmail.com>
2023-10-17 20:06:30 +0330

    little changes in model and created Q2.ipynb

--- Models.py
+++ Models.py
@@ -3,27 +3,37 @@ import numpy as np
 
 class Perceptron:
     _weights = None
-    _learning_rate = None
 
-    def __init__(self, input_size, learning_rate=0.01):
+    def __init__(self, input_size):
         self._weights = np.zeros(input_size)
-        self._learning_rate = learning_rate
 
-    def activation_function(self, x):
-        return max(0, x)
-        # return 1 if x >= 0 else 0
+    @staticmethod
+    def activation_function(x):
+        # return max(0, x)
+        return 1 if x >= 0 else 0
 
     def predict(self, inputs):
+        # check if inputs is a 2d numpy array or 1d
+        if len(inputs.shape) == 1:
+            inputs = np.array([inputs])
+
+        if len(inputs.shape) != 2:
+            raise Exception(f'Inputs shape {inputs.shape} is not supported')
+
+        if inputs.shape != self._weights.shape:
+            raise Exception(f'Inputs shape {inputs.shape} does not match weights shape {self._weights.shape}')
         weighted_sum = np.sum(np.multiply(inputs, self._weights))
         return self.activation_function(weighted_sum)
 
-    def train(self, inputs, labels, epochs=10):
+
+
+    def train(self, inputs, labels, epochs=10, learning_rate=0.1):
         for epoch_number in range(epochs):
             error_sum = 0
             for inputs_row, label in zip(inputs, labels):
                 prediction = self.predict(inputs_row)
                 error = (label - prediction)
                 error_sum += abs(error)
-                self._weights += self._learning_rate * error * inputs_row
+                self._weights += learning_rate * error * inputs_row
 
             print(f'Epoch: {epoch_number}, Error: {error_sum}')
